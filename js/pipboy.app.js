e79a6d29fad49b8b20f2f93b6e82fab1914db708
alessandrodirosa <76749449+alessandrodirosa@users.noreply.github.com>
2021-10-21 18:38:02 +0200

    Creato inventario e inizializzati alcuni oggetti di tipo armi

---
+++ js/pipboy.app.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    var weapons = [
+        {"name": "44_Pistol",
+        "damage": 48,
+        "range": 119,
+        "fireRate": 6,
+        "accuracy":66,
+        "weight": 4.2,
+        "value": 99},
+        {"name": "10mm_Pistol",
+        "damage": 18,
+        "range": 119,
+        "fireRate": 46,
+        "accuracy":61,
+        "weight": 1.2,
+        "value": 53},
+        {"name": "assault_Rifle",
+        "damage": 30,
+        "range": 119,
+        "fireRate": 40,
+        "accuracy":72,
+        "weight": 13.1,
+        "value": 144}
+    ];
+
+});
